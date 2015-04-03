c00764b9ac93abdf5a9deb764b81ad3811218f69
Jorge Antunes <ask@stoikerty.com>
2015-04-03 14:06:51 +0100

    add express server

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 2000,
+    publicDir = process.argv[2] || __dirname + '/public';
+
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
+});
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Project Server listening on http://%s:%s", hostname, port);
+app.listen(port, hostname);
