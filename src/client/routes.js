00e1f58046e7df286515aaec51bafa74468a55b3
Jorge Antunes <ask@stoikerty.com>
2016-03-19 12:43:37 +0000

    separate views used in router into individual files

--- src/client/routes.js
+++ src/client/routes.js
@@ -2,18 +2,13 @@ import React from 'react';
 import { Route } from 'react-router';
 
 import AppBody from './AppBody';
-
-const examplePage = ()=> (
-  <div> { 'Example Page' } </div>
-);
-const NoMatch404 = ()=> (
-  <div> { '404 - No route matches your request' } </div>
-);
+import ExamplePage from './views/ExamplePage';
+import NoMatch404 from './views/NoMatch404';
 
 export default (
   <Route path="/" component={AppBody}>
     {/* Make needed component available */}
-    <Route path="example-page" component={examplePage}/>
+    <Route path="example-page" component={ExamplePage}/>
 
     {/* Screen for non-existing routes */}
     <Route path="*" component={NoMatch404}/>
