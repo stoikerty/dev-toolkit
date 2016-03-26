a4efbb4b856f8b6bd0305766f36a7856251bba78
Jorge Antunes <stoikerty@gmail.com>
2016-03-26 13:31:52 +0000

    add more comments

--- toolkit/webpack/development.js
+++ toolkit/webpack/development.js
@@ -3,13 +3,15 @@ import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 
 import config from './config';
+const compiler = webpack(config);
 
 // Use the express production server
 import '../../src/server/app';
 
-const compiler = webpack(config);
-
+// Use middleware for hot-reloading
 app.server.instance.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.server.instance.use(webpackHotMiddleware(compiler));
 
-app.server.start({message: '==> Browsersync should be launched soon. Use one of the Access URLs for development.'});
+app.server.start({
+  message: '==> Browsersync should be launched soon. Use one of the Access URLs for development.'
+});
