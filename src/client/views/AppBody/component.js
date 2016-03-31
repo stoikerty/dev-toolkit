bc6ad3e6c5d03c76e2e23382a6063a97fb5c0def
Jorge Antunes <stoikerty@gmail.com>
2016-03-31 17:02:51 +0100

    update example-component with better styling and interactive click-example

--- src/client/views/AppBody/component.js
+++ src/client/views/AppBody/component.js
@@ -1,18 +1,36 @@
 import React from 'react';
+import cx from 'classnames';
 import s from './component.scss';
 
 export default class Component extends React.Component{
   constructor(props){
     super(props);
     this.displayName = 'Component';
+    this.toggleText = this.toggleText.bind(this);
 
-    this.state = {};
+    this.state = {
+      textEnabled: true,
+    };
+  }
+
+  toggleText(){
+    this.setState({ textEnabled : !this.state.textEnabled });
   }
 
   render() {
     return (
-      <div className={s.ExampleComponent}>
-        Styled Example Component
+      <div className={s.exampleComponent} onClick={this.toggleText}>
+        { 'Styled Example Component' }
+
+        <If condition={this.state.textEnabled}>
+          <div className={s.text}>
+            { 'click to test <If/> condition' }
+          </div>
+        <Else/>
+          <div className={cx(s.text, s.isHidden)}>
+            { 'woohooo!' }
+          </div>
+        </If>
       </div>
     );
   }
