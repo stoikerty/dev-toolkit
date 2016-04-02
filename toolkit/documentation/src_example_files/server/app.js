58eb410d535a4549093ca460e1b3069ea47f478b
Jorge Antunes <stoikerty@gmail.com>
2016-04-02 18:25:43 +0100

    create a class for making server use via toolkit clearer

--- toolkit/documentation/src_example_files/server/app.js
+++ toolkit/documentation/src_example_files/server/app.js
@@ -1,34 +1,17 @@
-import serveStatic from 'serve-static';
-import router from './router';
-
-// Preconfigure the `app`-object
+// Preconfigure the global `app`-object
 import './bootstrap';
 
-// launch server later with webpack using exported serverApp class
-export default class serverApp{
-  // Use this to start your server
-  start(options={ message:null }){
-    // serve build folder
-    app.server.instance.use(serveStatic('build'));
-
-    // Listen on specified port
-    app.server.instance.listen(app.server.port, function(error) {
-      if (error) {
-        app.error(error);
-      } else {
-        app.log('==> 🌎 Listening on http://' + app.server.hostname + ':%s/', app.server.port);
-        if (options.message){
-          app.log(options.message);
-        }
-      }
-    });
-
-    // start react-router
-    app.server.instance.use(router);
+// Launch server later with webpack using an exported class
+// The class needs `start` & `use`-methods
+export default class {
+  // Use this to start the server
+  start(options={ message: null }){
+    app.start({ message: options.message });
   }
 
-  // Redirect the middleware for webpack
+  // Bind the express middleware for webpack, this method should
+  // redirect to & act in the same way as the express `use`-method.
   use(...middlewareOptions){
-    app.server.instance.use(...middlewareOptions);
+    app.use(...middlewareOptions);
   }
 }
--- toolkit/documentation/src_example_files/server/bootstrap.js
+++ toolkit/documentation/src_example_files/server/bootstrap.js
@@ -1,27 +1,68 @@
 import express from 'express';
 import hbs from 'express-handlebars';
+import serveStatic from 'serve-static';
+
+import router from './router';
+
+class App{
+  constructor(){
+    // server config
+    this.hostname = 'localhost';
+    this.port = 2000;
+    // bind class methods
+    this.init = this.init.bind(this);
+    this.start = this.start.bind(this);
+    this.use = this.use.bind(this);
+
+    // Create express server instance & initialize
+    this.express = express();
+    this.init();
+
+    // Create application logging functionality.
+    // A neat console wrapper that keeps the correct line number.
+    // http://stackoverflow.com/questions/13815640/a-proper-wrapper-for-console-log-with-correct-line-number
+    /*eslint-disable no-console */
+    this.log = function() {
+      return console.log.bind(console);
+    }();
+    /*eslint-enable no-console */
+  }
+
+  init(){
+    // -> Set the folder for html views & disable powered-by header
+    this.express.engine('html', hbs({ extname: 'html' }));
+    this.express.set('views', process.cwd() + '/src/server/views');
+    this.express.set('view engine', 'html');
+    this.express.locals.settings['x-powered-by'] = false;
+    this.express.disable('x-powered-by');
+  }
+
+  // Launch express server with react-router
+  start(options={ message: null }){
+    // serve build folder
+    this.express.use(serveStatic('build'));
+
+    // Listen on specified port
+    this.express.listen(this.port, function(error) {
+      if (error) {
+        app.error(error);
+      } else {
+        app.log('==> 🌎 Listening on http://' + this.hostname + ':%s/', this.port);
+        if (options.message){
+          app.log(options.message);
+        }
+      }
+    });
+
+    // start react-router
+    this.express.use(router);
+  }
+
+  // bind express middleware
+  use(...middlewareOptions){
+    this.express.use(...middlewareOptions);
+  }
+}
 
 // Create Node Global
-GLOBAL.app = {};
-
-app.server = {};
-app.server.hostname = 'localhost';
-app.server.port = 2000;
-
-// Create express server instance
-// -> Set the folder for html views & disable powered-by header
-app.server.instance = express();
-app.server.instance.engine('html', hbs({ extname: 'html' }));
-app.server.instance.set('views', process.cwd() + '/src/server/views');
-app.server.instance.set('view engine', 'html');
-app.server.instance.locals.settings['x-powered-by'] = false;
-app.server.instance.disable('x-powered-by');
-
-/*eslint-disable no-console */
-// Create application logging functionality.
-// A neat console wrapper that keeps the correct line number.
-// http://stackoverflow.com/questions/13815640/a-proper-wrapper-for-console-log-with-correct-line-number
-app.log = function() {
-  return console.log.bind(console);
-}();
-/*eslint-enable no-console */
+GLOBAL.app = new App;
