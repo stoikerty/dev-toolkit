d2435c04febea60c200168be5a58f45b35d7b9da
Jorge Antunes <ask@stoikerty.com>
2016-04-04 16:13:07 +0100

    use correct `class`-name for `Home.js`

--- toolkit/documentation/src_example_files/client/views/AppBody/Home.js
+++ toolkit/documentation/src_example_files/client/views/AppBody/Home.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default class ExamplePage extends React.Component{
+export default class Home extends React.Component{
   constructor(props){
     super(props);
-    this.displayName = 'ExamplePage';
+    this.displayName = 'Home';
 
     this.state = {};
   }
