344a4b2574f7a5816359929e05736de5bbd01758
Jorge Antunes <stoikerty@gmail.com>
2016-05-22 02:04:02 +0100

    fix eslint issue

--- toolkit/webpack/generateBuild.js
+++ toolkit/webpack/generateBuild.js
@@ -5,7 +5,7 @@ global.usesServerRendering = false;
 
 // compile all files necessary for serving
 const compiler = webpack(config);
-compiler.run((err, stats)=>{
+compiler.run((err, stats) => {
   // output what's happening within webpack
-  console.log(stats.toString(config.stats));
+  console.log(stats && stats.toString(config.stats) || '');
 });
