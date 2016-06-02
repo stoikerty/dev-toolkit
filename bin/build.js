f0dc314f5ae69a08a4026da2cefc68392da168a2
Jorge Antunes <stoikerty@gmail.com>
2016-06-02 21:34:15 +0100

    use cash instead of shelljs

--- bin/build.js
+++ bin/build.js
@@ -1,2 +1,15 @@
 #! /usr/bin/env node
-console.log('build');
+var getInstalledPath = require('get-installed-path');
+var path = require('path');
+var $ = require('cash');
+
+// Get the application and module paths
+var appPath = process.cwd();
+var modulePath = getInstalledPath('universal-dev-toolkit');
+
+// Use the paths dynamically
+$('export UDT_APP_PATH=' + appPath);
+$('export UDT_ROOT=' + modulePath);
+$('export BABEL_ROOT_IMPORT_CUSTOM_ROOT_PATH=' + appPath);
+$('cd ' + modulePath);
+$('npm run build');
--- bin/dev.js
+++ bin/dev.js
@@ -1,19 +1,15 @@
 #! /usr/bin/env node
 var getInstalledPath = require('get-installed-path');
-var shell = require('shelljs');
 var path = require('path');
+var $ = require('cash');
 
+// Get the application and module paths
 var appPath = process.cwd();
 var modulePath = getInstalledPath('universal-dev-toolkit');
 
-shell.env['UDT_APP_PATH'] = appPath;
-shell.env['UDT_ROOT'] = modulePath;
-shell.env['BABEL_ROOT_IMPORT_CUSTOM_ROOT_PATH'] = appPath;
-// console.log(path.join(shell.env['UDT_APP_PATH'], '/src/server/app'));
-
-var command = 'npm run dev';
-// var devPath = path.join(modulePath, '/toolkit/webpack/development.js');
-// var command = 'babel-node ' + devPath;
-console.log(command);
-shell.cd(modulePath);
-shell.exec(command);
+// Use the paths dynamically
+$('export UDT_APP_PATH=' + appPath);
+$('export UDT_ROOT=' + modulePath);
+$('export BABEL_ROOT_IMPORT_CUSTOM_ROOT_PATH=' + appPath);
+$('cd ' + modulePath);
+$('npm run dev');
