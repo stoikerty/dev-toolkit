71c078613724eb5a1dcb1a628f9dfaf4e82c5d70
Jorge Antunes <ask@stoikerty.com>
2016-07-28 11:33:14 +0100

    '--color' adds color for OSX... who knew?? thankfully I found this: http://stackoverflow.com/questions/31620298/is-it-possible-for-child-processes-in-node-js-to-preserve-colored-output

--- bin/bootstrap.js
+++ bin/bootstrap.js
@@ -16,7 +16,7 @@ console.log('0.3 startScriptPath: ', startScriptPath);
 // https://lostechies.com/derickbailey/2014/02/20/how-i-work-around-the-require-problem-in-nodejs/
 spawn(
   'node',
-  [startScriptPath],
+  [startScriptPath, '--color'],
   {
     env: {
       NODE_PATH: currentPath,
