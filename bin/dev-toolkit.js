6b73b760b65a70ca365191d00e794071b3865695
Jorge Antunes <ask@stoikerty.com>
2016-07-30 14:54:21 +0100

    make bootstrap-script more comprehensive

--- bin/dev-toolkit.js
+++ bin/dev-toolkit.js
@@ -8,6 +8,7 @@ const argv = require('yargs')
 const pkg = require('../package.json');
 const debug = require('./utils/debug');
 
+// Notify of debug mode
 if (argv.debug) {
   process.env.TOOLKIT_DEBUG = true;
   console.log(chalk.magenta.underline('DEBUG MODE'));
@@ -20,47 +21,56 @@ function run(options) {
 
   debug('Toolkit Version', pkg.version);
   debug('Platform', process.platform);
-  debug('running Script', options.script);
   debug('NODE_PATH', process.env.NODE_PATH);
   debug('currentPath', currentPath);
   debug('devToolkitPath', devToolkitPath);
+  debug('');
+
+  debug('running Script:', options.script);
+  var args = [ path.resolve(__dirname, 'scripts/' + options.script + '.js') ];
+
+  debug('...with arguments:', options.args);
+  args = args.concat(options.args);
+
+  // Add color support for dependency-modules like `chalk`
+  args.push('--color');
+
+  // spawn is required for root-relative imports to work in server-rendering, because webpack's
+  // alias is not picked up in node. For other solutions, see the following:
+  // https://gist.github.com/branneman/8048520
+  // https://lostechies.com/derickbailey/2014/02/20/how-i-work-around-the-require-problem-in-nodejs/
+  spawn(
+    'node',
+    args,
+    {
+      env: {
+        NODE_PATH: currentPath,
+      },
+
+      // OSX will throw error if shell is not set
+      shell: !isWin,
+      stdio: 'inherit',
+    }
+  );
+
 
   if (argv.debug) {
     console.log('0.4 NODE_PATH after: ', process.env.NODE_PATH);
   }
-
-  // Required for root-relative imports to work in server-rendering because webpack's alias
-  // is not picked up in node. For other solutions, see the following.
-  // https://gist.github.com/branneman/8048520
-  // https://lostechies.com/derickbailey/2014/02/20/how-i-work-around-the-require-problem-in-nodejs/
-  // spawn(
-  //   'node',
-  //   // Add color support for dependency-modules like `chalk`
-  //   [devToolkitPath, '--color'],
-  //   {
-  //     env: {
-  //       NODE_PATH: currentPath,
-  //     },
-  //     shell: !isWin,
-  //     stdio: 'inherit',
-  //   }
-  // );
-
-  console.log(options);
-
-  // require('./start-commands/' + command);
 }
 
 if (argv.version) {
   console.log('[', pkg.name, pkg.version, ']');
 } else {
-  const command = process.argv[2];
-  switch (command) {
-    case 'init':
-      run({ script: command, arguments: { init: argv.init, debug: argv.debug } });
-      break;
-    default:
-      run({ script: 'development', arguments: { debug: argv.debug } });
-      break;
+  if (argv.init) {
+    run({ script: 'init', args: [argv.init] });
+  // if (argv.build) {
+  //   run({ script: 'build', args: [argv.build] });
+  // if (argv.test) {
+  //   run({ script: 'test', args: [argv.test] });
+  // if (argv.eject) {
+  //   run({ script: 'eject', args: [argv.eject] });
+  } else {
+    run({ script: 'development', args: [] });
   }
 }
