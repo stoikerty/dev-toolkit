e0a541e6bbf60e842b80641cc80e7f61c4e48191
Jorge Antunes <ask@stoikerty.com>
2016-08-01 11:58:21 +0100

    add ability to resolve loaders when installed as a dependency

--- toolkit/webpack/config/resolveLoader.js
+++ toolkit/webpack/config/resolveLoader.js
@@ -1,11 +1,13 @@
 import path from 'path';
 
 import {
+  rootForProject,
   rootForToolkit,
 } from '../../_userSettings';
 
 export default {
   modulesDirectories: [
+    path.resolve(rootForProject, 'node_modules'),
     path.resolve(rootForToolkit, 'node_modules'),
   ],
 };
