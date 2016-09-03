0aca348c8ca1eaa8bc544b2c6541d3943137ae7b
Jorge Antunes <ask@stoikerty.com>
2016-09-03 22:31:46 +0100

    improve starting point boilerplate

--- starting-point/src/client/routes.jsx
+++ starting-point/src/client/routes.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
-import Shell from './Shell';
-
-const Page = () => (
-  <div>{'some text'}</div>
-);
+import Shell from './views/Shell';
+import Home from './views/features/Home';
 
 // `routes.js`
 // -----------
@@ -13,11 +10,15 @@ const Page = () => (
 // These routes are rendered both on the client and on the server, therefore
 // they are imported in `RouterContainer` and in `server/router.js`.
 
+// NOTE: Even if you think you "don't need" server-rendering, you probably will do
+//   if you have any intention of statically rendering your markup and using
+//   react-router's dynamic routing.
+
 // Use Shell as surrounding container for all routes
 export default (
   <Route path="/" component={Shell}>
 
     {/* A view for the home page */}
-    <IndexRoute component={Page} />
+    <IndexRoute component={Home} />
   </Route>
 );
--- starting-point/src/client/views/Shell.jsx
+++ starting-point/src/client/views/Shell.jsx
@@ -2,6 +2,11 @@ import React, { PropTypes } from 'react';
 
 import s from './Shell/_style.scss';
 
+// `Shell.js`
+// --------------------
+// Having a Shell is perfect if you want to transition between different features of your app or
+// managing scroll-state and navigation.
+
 const displayName = 'Shell';
 const defaultProps = {};
 const propTypes = {
