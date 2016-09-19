0b4924fbd92eec6e7cb24700c1aadc9077efc3eb
Jorge Antunes <ask@stoikerty.com>
2016-09-19 18:25:44 +0100

    start using dynamic-pages

--- toolkit/build/generateStaticFiles.js
+++ toolkit/build/generateStaticFiles.js
@@ -3,6 +3,7 @@ import path from 'path';
 import each from 'lodash.foreach';
 import mkdirp from 'mkdirp';
 import chalk from 'chalk';
+import DynamicPages from 'dynamic-pages';
 
 // TODO:
 //  - cover case of `renderedRoutes` without dynamic component(s)
@@ -11,5 +12,26 @@ import chalk from 'chalk';
 
 // Compile all files necessary for serving
 export default (staticRender, PATHS, message) => {
+  const indexHtml = path.resolve(PATHS.buildFolder, 'index.html');
+  const manifestFile = PATHS.manifest;
   console.log('generating static files...', staticRender, PATHS, message);
+
+
+  fs.readFile(manifestFile, 'utf8', (manifestError, manifestData) => {
+    if (manifestError) throw manifestError;
+
+    fs.readFile(indexHtml, 'utf8', (readError, data) => {
+      if (readError) throw readError;
+
+      const definedRoutes = DynamicPages.getDefinedRoutes();
+
+      definedRoutes.forEach((definedRoute) => {
+        const { renderPath, components } = definedRoute;
+
+        // TODO: make directory with corresponding index.html file
+        // const routeFolder = path.resolve(PATHS.build, renderPath.substring(1));
+        // mkdirp(routeFolder, (mkdirError) => {});
+      });
+    });
+  });
 };
