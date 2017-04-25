7fe9218f25f8e2dd9136e96f6410127b44d0ea9a
Jorge Antunes <ask@stoikerty.com>
2017-04-25 21:38:43 +0100

    try a neutrino async project

---
+++ test-projects/react-code-splitting/src/components/Home.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Home = (props) => <h1>{props.name}</h1>;
+
+export default Home;
---
+++ test-projects/react-code-splitting/src/components/Page2.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Page2 = (props) => <h1>{props.name}</h1>;
+
+export default Page2;
