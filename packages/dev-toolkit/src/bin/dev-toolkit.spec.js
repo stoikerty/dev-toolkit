c34cf6baf4114f91983a6148e62a22f3077ac2b8
Jorge Antunes <ask@stoikerty.com>
2017-06-24 21:29:38 +0100

    add dev-toolkit command tests + sandbox utility

--- packages/dev-toolkit/src/bin/dev-toolkit.spec.js
+++ packages/dev-toolkit/src/bin/dev-toolkit.spec.js
@@ -1,7 +1,49 @@
-import './dev-toolkit';
+import { expect } from 'chai';
+import yargs from 'yargs';
+
+import sandbox from 'dist/utilities/testHelpers/sandbox';
+
+import devToolkit from './dev-toolkit';
 
 describe('dev-toolkit', () => {
-  it('runs the watch script when given `--watch` or `-w` argument', () => {
-    console.log('it runs!');
+  const previousEnv = process.env.TOOLKIT_DEBUG;
+  sandbox.use(before, after);
+
+  afterEach(() => {
+    process.env.TOOLKIT_DEBUG = previousEnv;
+  });
+
+  describe('runs the toolkit in debug-mode', () => {
+    it('when given `-d` argument', () => {
+      const runCommand = sandbox.spy();
+
+      devToolkit({ cmdArgs: ['dev-toolkit', '-d'], runCommand });
+      expect(process.env.TOOLKIT_DEBUG).to.equal('true');
+    });
+    it('when given `--debug` argument', () => {
+      const runCommand = sandbox.spy();
+
+      devToolkit({ cmdArgs: ['dev-toolkit', '--debug'], runCommand });
+      expect(process.env.TOOLKIT_DEBUG).to.equal('true');
+    });
+  });
+
+  it('outputs current version when given `-v` or `--version` argument', () => {
+  });
+
+  it('runs the watch script when given `-w` or `--watch` argument', () => {
+    const runCommand = sandbox.spy();
+    const processedArgs = yargs.alias('w', 'watch').parse(['dev-toolkit', '-w']);
+    const expectedOutput = {
+      script: 'watch',
+      message: 'Watching files for development',
+      args: [processedArgs.watch],
+    };
+
+    devToolkit({ cmdArgs: ['dev-toolkit', '-w'], runCommand });
+    expect(runCommand.calledWith(expectedOutput)).to.equal(true);
+
+    devToolkit({ cmdArgs: ['dev-toolkit', '--watch'], runCommand });
+    expect(runCommand.calledWith(expectedOutput)).to.equal(true);
   });
 });
