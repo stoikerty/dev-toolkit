6982e7d50e22a4b0ce86a2eccc81d2df95982448
Jorge Antunes <ask@stoikerty.com>
2017-06-24 18:06:32 +0100

    add minimal starting point

---
+++ packages/dev-toolkit/starting-point/minimal/src/client/app.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import domready from 'domready';
+
+const hotReRender = () => {
+  // Dynamically require module inline for hot-reloading
+  import('./views/Shell').then((Shell) => {
+    ReactDOM.render(
+      <Shell />,
+      document.querySelector('[data-jshook~="app-body"]'),
+    );
+  });
+};
+
+// Start app by rendering it for the first time, only after DOM has loaded.
+domready(
+  () => hotReRender(),
+);
+
+// Support hot-reloading of components by rerendering using webpack's included HMR.
+// HMR stands for "Hot-Module-Replacement", also called "vanilla HMR"
+if (module.hot) {
+  module.hot.accept('./views/Shell', () => {
+    setTimeout(hotReRender);
+  });
+}
---
+++ packages/dev-toolkit/starting-point/minimal/src/server/app.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import hbs from 'express-handlebars';
+import path from 'path';
+
+import { isDev } from 'src/settings';
+
+// Unlike the client app, the server app can only ever be run on the server,
+// we therefore have direct access to Node-specific things like `process`
+const serverPort = process.env.SERVER_PORT || 2000;
+const rootDirectory = process.cwd();
+const buildDir = path.join(rootDirectory, 'build');
+const serverViews = `${rootDirectory}/src/server/views`;
+
+export default class {
+  constructor() {
+    this.express = express();
+
+    // Start up server immediately if we're not using the server via webpack
+    if (!isDev) {
+      this.start();
+    }
+  }
+
+  // Ability to launch server later (allows webpack to bind middleware before start)
+  start() {
+    // We're using handlebars as the renderer for the layout.hbs-file in `src/server/views`
+    this.express.engine('hbs', hbs({ extname: 'hbs' }));
+    this.express.set('views', serverViews);
+    this.express.set('view engine', 'hbs');
+    this.express.locals.settings['x-powered-by'] = false;
+    this.express.disable('x-powered-by');
+
+    // We tell express that it should serve all files statically
+    this.express.use(express.static(buildDir));
+
+    // A simple health-check endpoint to see if the server is alive
+    this.express.get('/health', (req, res) => {
+      res.send('OK');
+    });
+
+    // Run the express server by listening to our specified port
+    this.serverInstance = this.express.listen(serverPort, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Server is listening on port ${serverPort}`);
+    });
+  }
+
+  // A way to stop and shut-down the server, in case you need it for things like tests
+  stop() {
+    this.serverInstance.close();
+  }
+
+  // Bind the express middleware so webpack can use it to attach the dev-server middleware,
+  // this method should redirect to & act in the same way as the express `use`-method.
+  use(...middlewareOptions) {
+    this.express.use(...middlewareOptions);
+  }
+}
