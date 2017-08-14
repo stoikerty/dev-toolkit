b1f17daaedf2751ac75c60958082c6a50fdbf9db
Jorge Antunes <ask@stoikerty.com>
2017-08-15 00:57:40 +0100

    remove remaining old scripts

--- packages/dev-toolkit/src/bin/scripts_old/serveStatic.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import '../utils/bootstrap';
-
-global.toolkitScript = 'serveStatic';
-global.scriptOptions = {};
-
-console.log('run serveStatic command');
-// require('../../commands/serveStatic');
