930dcd357bc6a59ececeac2746770c79cfb7379b
Jorge Antunes <ask@stoikerty.com>
2017-09-13 09:20:58 +0100

    start adding help to command-line utility

--- packages/dev-toolkit/src/bin/index.js
+++ packages/dev-toolkit/src/bin/index.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 import yargs from 'yargs';
 import spawn from 'cross-spawn';
-import chalk from 'chalk';
 import path from 'path';
 import babelRunner from 'babel-runner';
 
 import { log } from '../utilities';
-import pkg from '../../package.json';
 
 const runCommand = ({ command, message, args }) => {
   if (command === 'run') {
@@ -44,63 +42,96 @@ const runCommand = ({ command, message, args }) => {
 };
 
 const devToolkit = ({ cmdArgs }) => {
-  const processedArgs = yargs
-    .alias('r', 'run')
-    .alias('w', 'watch')
-    .alias('b', 'build')
-    .alias('i', 'init')
-    .alias('s', 'serve')
-    .alias('static', 'serve-static')
+  yargs
+    .usage('\nUsage: dev-toolkit <command> [options]')
+
+    .command({
+      command: 'build',
+      aliases: ['build', 'b'],
+      desc: 'Generates a static build',
+      handler: (argv) => (runCommand({
+        command: 'build',
+        message: 'Generating a static build',
+        argv,
+      })),
+    })
+
+    .command({
+      command: 'version',
+      aliases: ['version', 'v'],
+      desc: 'Outputs current version number',
+      handler: (argv) => (runCommand({
+        command: 'version',
+        message: 'Output current version number',
+        argv,
+      })),
+    })
+
+    .command({
+      command: 'watch',
+      aliases: ['watch', 'w'],
+      desc: 'Watches files for development',
+      handler: (argv) => (runCommand({
+        command: 'watch',
+        message: 'Watching files for development',
+        argv,
+      })),
+    })
+
+    .help()
+    .argv;
+
+    // .alias('b', 'build')
+    // .alias('i', 'init')
+    // .alias('s', 'serve')
+    // .alias('v', 'version')
+    // .alias('w', 'watch')
+    // .alias('r', 'run')
+  yargs
     .parse(cmdArgs);
 
-  // Outputs current version number from `package.json`
-  if (processedArgs.v || processedArgs.version) {
-    log({ title: 'version', message: `You are running ${chalk.green(`${pkg.name} v${pkg.version}`)}\n` });
-  }
-
   // Runs corresponding command inside `src/commands`-folder
-  if (processedArgs.run) {
-    runCommand({
-      command: 'run',
-      args: [processedArgs.run],
-    });
-  }
-  if (processedArgs.watch) {
-    runCommand({
-      command: 'watch',
-      message: 'Watching files for development',
-      args: [processedArgs.watch],
-    });
-  }
-  if (processedArgs.serve) {
-    runCommand({
-      command: 'serve',
-      message: 'Watching files for development',
-      args: [processedArgs.serve],
-    });
-  }
-  if (processedArgs['serve-static']) {
-    log({ message: 'This command is not meant for production use.', type: 'warning' });
-    runCommand({
-      command: 'serveStatic',
-      message: 'Serving the /build folder using a minimal server',
-      args: [processedArgs['serve-static']],
-    });
-  }
-  if (processedArgs.init) {
-    runCommand({
-      command: 'init',
-      message: 'Initializing new project',
-      args: [processedArgs.init],
-    });
-  }
-  if (processedArgs.build) {
-    runCommand({
-      command: 'build',
-      message: `Creating a static build${(processedArgs.dynamic ? ' with dynamic pages' : '')}`,
-      args: [(processedArgs.dynamic ? 'dynamic' : '')],
-    });
-  }
+  // if (processedArgs.build) {
+  //   runCommand({
+  //     command: 'build',
+  //     message: 'Creating a static build',
+  //     args: [processedArgs],
+  //   });
+  // }
+  // if (processedArgs.init) {
+  //   runCommand({
+  //     command: 'init',
+  //     message: 'Initializing new project',
+  //     args: [processedArgs.init],
+  //   });
+  // }
+  // if (processedArgs.serve) {
+  //   runCommand({
+  //     command: 'serve',
+  //     message: 'Watching files for development',
+  //     args: [processedArgs.serve],
+  //   });
+  // }
+  // if (processedArgs.version) {
+  //   runCommand({
+  //     command: 'version',
+  //     message: 'Output current version number',
+  //     args: [processedArgs.version],
+  //   });
+  // }
+  // if (processedArgs.watch) {
+  //   runCommand({
+  //     command: 'watch',
+  //     message: 'Watching files for development',
+  //     args: [processedArgs.watch],
+  //   });
+  // }
+  // if (processedArgs.run) {
+  //   runCommand({
+  //     command: 'run',
+  //     args: [processedArgs.run],
+  //   });
+  // }
 };
 
 // Run toolkit immediately using yargs if we're not testing it
