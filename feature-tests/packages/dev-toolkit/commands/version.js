e8ff458bbc36e951fa4914d4d98c33f0300eb6df
Jorge Antunes <ask@stoikerty.com>
2017-09-20 09:29:31 +0100

    fix test

--- feature-tests/packages/dev-toolkit/commands/version.js
+++ feature-tests/packages/dev-toolkit/commands/version.js
@@ -2,9 +2,9 @@ import { expect } from 'chai';
 import shell from 'shelljs';
 
 export default ({ pkg }) => {
-  describe('version command `dev-toolkit -v`', () => {
+  describe('version command `dev-toolkit version`', () => {
     it('runs without failure & outputs version number', (done) => {
-      shell.exec('node ./node_modules/.bin/dev-toolkit --version', { silent: true }, (code, stdout) => {
+      shell.exec('node ./node_modules/.bin/dev-toolkit version', { silent: true }, (code, stdout) => {
         expect(code, 'Exit code').to.equal(0);
         expect((stdout.indexOf(pkg.version) >= 0)).to.equal(true);
         done();
