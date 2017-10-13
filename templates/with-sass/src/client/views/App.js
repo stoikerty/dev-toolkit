9d570598f1c9af83b6d230d6531e40b7319d1994
Jorge Antunes <ask@stoikerty.com>
2017-10-13 14:33:05 +0100

    add `with-sass` template

---
+++ templates/with-sass/src/client/views/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import s from './style.scss';
+
+// Use this as the entry-point for your app.
+export default () => (
+  <div className={s.app}>
+    {'App with Sass'}
+    <br />
+    <br />
+    <span className={s.newColor}>{'so '}</span>
+    <span className={s.color2}>{'colorful '}</span>
+    <span className={s.color3}>{'! '}</span>
+    <br />
+    <span className={s.color4}>{'and '}</span>
+    <span className={s.wiggleLeftRight}>{'with other cool things'}</span>
+    <span className={s.color6}>{' like '}</span>
+    <span className={s.wiggleUpDown}>{'animations'}</span>
+    <span> </span>
+    <span className={s.wiggleRotate}>{'!'}</span>
+  </div>
+);
