36baa3f47207bd839e963d534f12ffd2fe8ce1f4
Jorge Antunes <ask@stoikerty.com>
2017-10-14 12:20:40 +0100

    babel-preset-dev-toolkit: Better comments, use prettier

--- packages/babel-preset-dev-toolkit/index.js
+++ packages/babel-preset-dev-toolkit/index.js
@@ -1,25 +1,26 @@
-"use strict";
+'use strict';
 
 // Use require.resolve to prevent issues when using npm link
 // see: https://github.com/babel/babel-loader/issues/149
 
 module.exports = {
   presets: [
-    require.resolve("babel-preset-env"),
+    require.resolve('babel-preset-env'),
     // Use create-react-app default
-    require.resolve("babel-preset-react-app")
+    require.resolve('babel-preset-react-app'),
   ],
   plugins: [
-    // Support `import()`-statement
-    require.resolve("babel-plugin-dynamic-import-node"),
+    // Support dynamic `import()`-statement
+    require.resolve('babel-plugin-dynamic-import-node'),
     // Sane if-statements for React
-    require.resolve("jsx-control-statements"),
+    require.resolve('jsx-control-statements'),
     // Allow root-relative imports for client & server
     [
-      require.resolve("babel-plugin-module-resolver"),
+      require.resolve('babel-plugin-module-resolver'),
       {
-        root: [process.cwd()]
-      }
-    ]
-  ]
+        // using `process.cwd` makes it also work with `import()`
+        root: [process.cwd()],
+      },
+    ],
+  ],
 };
