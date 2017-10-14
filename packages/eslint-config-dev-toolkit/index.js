4dcf555f5bb0609056ac321c81d4d2f4f7a9a749
Jorge Antunes <ask@stoikerty.com>
2017-10-14 20:19:57 +0100

    add lint commands, process files

--- packages/eslint-config-dev-toolkit/index.js
+++ packages/eslint-config-dev-toolkit/index.js
@@ -1,25 +1,25 @@
-"use strict";
+'use strict';
 
 // Use require.resolve to prevent issues when using npm link
 // see: https://github.com/babel/babel-loader/issues/149
 
 module.exports = {
   rules: {
-    "react/jsx-filename-extension": 0,
-    "import/no-unresolved": [
-      "error",
+    'react/jsx-filename-extension': 0,
+    'import/no-unresolved': [
+      'error',
       {
-        ignore: ["src/"]
-      }
+        ignore: ['src/'],
+      },
     ],
-    "import/no-extraneous-dependencies": ["off"],
-    "import/extensions": ["off", "never"]
+    'import/no-extraneous-dependencies': ['off'],
+    'import/extensions': ['off', 'never'],
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   settings: {
-    "import/resolver": {
-      "babel-module": {}
-    }
+    'import/resolver': {
+      'babel-module': {},
+    },
   },
-  env: { browser: true }
+  env: { browser: true },
 };
