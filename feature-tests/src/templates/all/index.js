07fb3ab31d0834e34440d67628308b35ca3a2448
Jorge Antunes <ask@stoikerty.com>
2017-10-14 17:02:38 +0100

    add longer timeout to default template a well

--- feature-tests/src/templates/all/index.js
+++ feature-tests/src/templates/all/index.js
@@ -41,7 +41,7 @@ export default ({ name, isDefault } = { isDefault: false }) => {
           logOutput(output);
           done();
         });
-      });
+      }).timeout(time('2 minutes').ms());
       it('with no specified template, skipping comments', done => {
         runDevToolkitCli({
           command: `init ${name}_default_app_no_comment --skipComments`,
@@ -50,7 +50,7 @@ export default ({ name, isDefault } = { isDefault: false }) => {
           logOutput(output);
           done();
         });
-      });
+      }).timeout(time('2 minutes').ms());
     }
   });
 };
