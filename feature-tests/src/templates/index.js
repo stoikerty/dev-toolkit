e695ea85b07135f991bf72e53b2197b6ae0e6710
Jorge Antunes <ask@stoikerty.com>
2017-10-14 16:25:00 +0100

    try a timeout of 2 minutes

--- feature-tests/src/templates/index.js
+++ feature-tests/src/templates/index.js
@@ -1,3 +1,5 @@
+import time from 'to-time';
+
 // Add your template to this array for it to be tested
 const templates = ['minimal', 'standard', 'with-sass', 'serverless'];
 const defaultTemplate = 'standard';
@@ -11,7 +13,7 @@ export default () => {
           const template = module.default;
           describe(`the ${name}-template:`, () => {
             template({ name, isDefault: name === defaultTemplate });
-          });
+          }).timeout(time('2 minutes').ms());
         })
         .catch(e => {
           console.log("combined template-tests couldn't load:\n", e);
