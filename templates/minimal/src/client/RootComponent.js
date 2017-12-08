a7a86ef126ab795039777584a0d2a5afb138626d
Jorge Antunes <ask@stoikerty.com>
2017-12-08 18:10:20 +0000

    improve the minimal template

--- templates/minimal/src/client/RootComponent.js
+++ templates/minimal/src/client/RootComponent.js
@@ -3,10 +3,9 @@
 // It's likely that this component will stay mostly empty since it sits at the root.
 // It might contain your chosen routing solution or redux-provider.
 import React from 'react';
-import App from './views/App';
 
 const displayName = 'RootComponent';
-const component = () => <App />;
+const component = () => <div>{'Minimal App'}</div>;
 
 component.displayName = displayName;
 export default component;
