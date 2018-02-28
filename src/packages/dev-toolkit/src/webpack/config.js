b4465720741676aa268a9c67a6cbfa01d629d9bd
Gil Filipe <gil.filipe@mindera.com>
2018-02-28 16:17:23 +0000

    Update logs config on webpack config

--- src/packages/dev-toolkit/src/webpack/config.js
+++ src/packages/dev-toolkit/src/webpack/config.js
@@ -103,25 +103,6 @@ export default ({ getWebpackAssets, creatingBuild, userSettings } = { creatingBu
           'node_modules',
         ],
       },
-
-      // devServer: {
-      //   stats: {
-      //     colors: true,
-      //     hash: false,
-      //     version: false,
-      //     timings: false,
-      //     assets: false,
-      //     chunks: false,
-      //     modules: false,
-      //     reasons: false,
-      //     children: false,
-      //     source: false,
-      //     errors: false,
-      //     errorDetails: false,
-      //     warnings: false,
-      //     publicPath: false,
-      //   },
-      // },
     },
 
     customizationOptions
