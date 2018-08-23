fef7e0388051d26b3030356932019af2fa7b3939
Jorge Antunes <jorgeantunes@stoikerty.com>
2018-08-23 13:27:52 +0100

    Correctly exit when error and update dependencies (#67)
    * correctly exit when an error occurs
    * update dependencies
    * bump version

--- src/packages/dev-toolkit/src/utilities/log/error.js
+++ src/packages/dev-toolkit/src/utilities/log/error.js
@@ -4,5 +4,8 @@ export default ({ error }) => {
   if (error) {
     const errorType = /^.*(Error: )/gi;
     console.log(`${chalk.red(error.stack.match(errorType))}${error.stack.replace(errorType, '')}`);
+
+    // Don't proceed further and exit with correct error code
+    process.exit(1);
   }
 };
