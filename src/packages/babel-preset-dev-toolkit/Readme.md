e3096cc253b72e53236b304dc45a9bff297643bb
Jorge Antunes <jorgeantunes@stoikerty.com>
2018-10-03 16:58:37 +0100

    stop using create-react-app preset to prevent sub-dependency issues (#73)

--- src/packages/babel-preset-dev-toolkit/Readme.md
+++ src/packages/babel-preset-dev-toolkit/Readme.md
@@ -1,3 +1,3 @@
 # babel-preset-dev-toolkit
 
-Babel Preset for [dev-toolkit](https://github.com/stoikerty/dev-toolkit), based on `create-react-app` preset.
+Babel Preset for [dev-toolkit](https://github.com/stoikerty/dev-toolkit).
--- src/packages/babel-preset-dev-toolkit/index.js
+++ src/packages/babel-preset-dev-toolkit/index.js
@@ -1,28 +1,20 @@
 'use strict';
 const path = require('path');
 
-// Warn Users to make sure we don't have an invalid `NODE_ENV`
-var env = process.env.BABEL_ENV || process.env.NODE_ENV;
-if (env !== 'development' && env !== 'test' && env !== 'production') {
-  throw new Error(
-    'Using `babel-preset-dev-toolkit` requires that you specify `NODE_ENV` or ' +
-      '`BABEL_ENV` environment variables. Valid values are "development", ' +
-      '"test", and "production". Instead, received: ' +
-      JSON.stringify(env) +
-      ".\nNOTE: `babel-preset-dev-toolkit` uses facebook's `babel-preset-react-app`" +
-      ' under the hood which has the same requirement.\n\n'
-  );
-}
-
 // Use require.resolve to prevent issues when using npm link
 // see: https://github.com/babel/babel-loader/issues/149
-module.exports = {
-  presets: [
-    require.resolve('babel-preset-env'),
-    // Use create-react-app default
-    require.resolve('babel-preset-react-app'),
-  ],
-  plugins: []
+var preset = {
+  presets: [require.resolve('babel-preset-env'), require.resolve('babel-preset-react')],
+  plugins: [
+    // class { handleThing = () => { } }
+    require.resolve('babel-plugin-transform-class-properties'),
+
+    // { ...todo, completed: true }
+    require.resolve('babel-plugin-transform-object-rest-spread'),
+
+    // support for async/await
+    require.resolve('babel-plugin-transform-runtime'),
+  ]
     .concat(
       env !== 'test'
         ? // Support dynamic `import()`-statement everywhere
@@ -46,3 +38,39 @@ module.exports = {
       ],
     ]),
 };
+
+// Warn Users to make sure we don't have an invalid `NODE_ENV`
+var env = process.env.BABEL_ENV || process.env.NODE_ENV;
+if (env !== 'development' && env !== 'test' && env !== 'production') {
+  throw new Error(
+    'Using `babel-preset-dev-toolkit` requires that you specify `NODE_ENV` or ' +
+      '`BABEL_ENV` environment variables. Valid values are "development", ' +
+      '"test", and "production". Instead, received: ' +
+      JSON.stringify(env) +
+      '.'
+  );
+}
+
+if (env === 'development' || env === 'test') {
+  preset.plugins.push.apply(preset.plugins, [
+    // Adds component stack to warning messages
+    require.resolve('babel-plugin-transform-react-jsx-source'),
+  ]);
+}
+
+if (env === 'test') {
+  preset.plugins.push.apply(preset.plugins, [
+    // Compiles import() to a deferred require()
+    require.resolve('babel-plugin-dynamic-import-node'),
+    // Transform ES modules to commonjs for Jest support
+    [require.resolve('babel-plugin-transform-es2015-modules-commonjs'), { loose: true }],
+  ]);
+}
+
+if (env === 'production') {
+  preset.plugins.push.apply(preset.plugins, [
+    require.resolve('babel-plugin-transform-react-remove-prop-types'),
+  ]);
+}
+
+module.exports = preset;
